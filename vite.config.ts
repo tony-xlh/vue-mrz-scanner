7dc13eed99570451195a18040175405d789ff391
xulihang <xulihanghai@163.com>
2023-08-01 13:53:11 +0800

    update vite.config.ts to build module

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/guide/build.html#library-mode
 export default defineConfig({
-  plugins: [vue()],
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: 'mrz-scanner',
+      fileName: 'mrz-scanner',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+  plugins: [vue(),dts()],
+});
